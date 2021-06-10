e52c5d2fce851d91849b5fb2174b0773be644e10
pfrost982 <power982@gmail.com>
2021-06-11 04:40:47 +0700

    добавлено тренировочное меню

--- app/src/main/java/ru/gb/notepad/MainActivity.java
+++ app/src/main/java/ru/gb/notepad/MainActivity.java
@@ -1,9 +1,13 @@
 package ru.gb.notepad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements NoticeFragment.Controller, NoticeListFragment.Controller {
     private Notepad notepad;
@@ -22,6 +26,32 @@ public class MainActivity extends AppCompatActivity implements NoticeFragment.Co
                 .beginTransaction()
                 .replace(R.id.container, NoticeListFragment.newInstance(notepad.getNoticeList()))
                 .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_example, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_favorite:
+                Toast.makeText(this, "action_favorite", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_main:
+                Toast.makeText(this, "action_main", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_search:
+                Toast.makeText(this, "action_search", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(this, "action_settings", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return true;
     }
 
     @Override
