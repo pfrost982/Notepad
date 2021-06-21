80f1ad519d974a0f7217855fa3dd2c9bbe23c0cc
pfrost982 <power982@gmail.com>
2021-06-21 18:52:36 +0700

    начинаю recyclerView

--- app/src/main/java/ru/gb/notepad/MainActivity.java
+++ app/src/main/java/ru/gb/notepad/MainActivity.java
@@ -1,14 +1,10 @@
 package ru.gb.notepad;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -19,64 +15,37 @@ public class MainActivity extends AppCompatActivity implements NoticeFragment.Co
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)
-                findViewById(R.id.bottom_navigation);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        int id = item.getItemId();
-                        switch (id) {
-                            case R.id.action_add:
-                                Log.d("@@@", "onNavigationItemSelected: add");
-                                return true;
-                            case R.id.action_settings:
-                                Log.d("@@@", "onNavigationItemSelected: settings");
-                                return true;
-                            case R.id.action_save:
-                                Log.d("@@@", "onNavigationItemSelected: save");
-                        }
-                        return true;
+                item -> {
+                    int id = item.getItemId();
+                    switch (id) {
+                        case R.id.action_add:
+                            Log.d("@@@", "onNavigationItemSelected: add");
+                            return true;
+                        case R.id.action_settings:
+                            Log.d("@@@", "onNavigationItemSelected: settings");
+                            return true;
+                        case R.id.action_save:
+                            Log.d("@@@", "onNavigationItemSelected: save");
                     }
+                    return true;
                 });
 
         notepad = new Notepad();
-
-        notepad.addNotice(new Notice(Notice.generateNewId(), "Магазин", "Купить морковку и картошку", Notice.getCurrentDate()));
-        notepad.addNotice(new Notice(Notice.generateNewId(), "Будильник", "Не забыть поставить будильник", Notice.getCurrentDate()));
-        notepad.addNotice(new Notice(Notice.generateNewId(), "Жена", "Не забыть позвонить жене", Notice.getCurrentDate()));
-        notepad.addNotice(new Notice(Notice.generateNewId(), "Ремонт", "Отремонтировать дверную ручку в детской", Notice.getCurrentDate()));
-        notepad.addNotice(new Notice(Notice.generateNewId(), "Уроки", "Дописать шестую домашнюю работу", Notice.getCurrentDate()));
+        initNotepad();
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.container, NoticeListFragment.newInstance(notepad.getNoticeList()))
                 .commit();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_example, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.action_favorite:
-                Toast.makeText(this, "action_favorite", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.action_main:
-                Toast.makeText(this, "action_main", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.action_search:
-                Toast.makeText(this, "action_search", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.action_settings:
-                Toast.makeText(this, "action_settings", Toast.LENGTH_SHORT).show();
-                return true;
-        }
-        return true;
+    private void initNotepad() {
+        notepad.addNotice(new Notice(Notice.generateNewId(), "Магазин", "Купить морковку и картошку", Notice.getCurrentDate()));
+        notepad.addNotice(new Notice(Notice.generateNewId(), "Будильник", "Не забыть поставить будильник", Notice.getCurrentDate()));
+        notepad.addNotice(new Notice(Notice.generateNewId(), "Жена", "Не забыть позвонить жене", Notice.getCurrentDate()));
+        notepad.addNotice(new Notice(Notice.generateNewId(), "Ремонт", "Отремонтировать дверную ручку в детской", Notice.getCurrentDate()));
+        notepad.addNotice(new Notice(Notice.generateNewId(), "Уроки", "Дописать шестую домашнюю работу", Notice.getCurrentDate()));
     }
 
     @Override
@@ -90,16 +59,10 @@ public class MainActivity extends AppCompatActivity implements NoticeFragment.Co
     @Override
     public void openNotice(Notice notice) {
         boolean isLandscape = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
-        if (isLandscape) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.detail_container, NoticeFragment.newInstance(notice))
-                    .commit();
-        } else {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.container, NoticeFragment.newInstance(notice))
-                    .commit();
-        }
+        getSupportFragmentManager()
+                .beginTransaction()
+                .addToBackStack(null)
+                .replace(isLandscape ? R.id.detail_container : R.id.container, NoticeFragment.newInstance(notice))
+                .commit();
     }
 }
