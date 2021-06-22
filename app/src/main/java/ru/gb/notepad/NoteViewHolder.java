62203ca2dd6d9c1797f1d63625e68da909f09ef7
pfrost982 <power982@gmail.com>
2021-06-23 01:57:09 +0700

    Добавлен DatePicker

--- app/src/main/java/ru/gb/notepad/NoteViewHolder.java
+++ app/src/main/java/ru/gb/notepad/NoteViewHolder.java
@@ -9,6 +9,8 @@ import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.Calendar;
+
 public class NoteViewHolder extends RecyclerView.ViewHolder {
     private final TextView titleTextView;
     private final TextView dateTextView;
@@ -30,6 +32,9 @@ public class NoteViewHolder extends RecyclerView.ViewHolder {
     public void bind(Notice notice) {
         this.notice = notice;
         titleTextView.setText(notice.title);
-        dateTextView.setText(Long.toString(notice.dateOfCreation));
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(notice.dateOfCreation);
+        dateTextView.setText(calendar.get(Calendar.DAY_OF_MONTH) + "." + calendar.get(Calendar.MONTH)
+                + "." + calendar.get(Calendar.YEAR));
     }
 }
